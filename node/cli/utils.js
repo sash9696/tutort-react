5e7e9d82c557f51d6f8e0ecb40824087922015d4
Sahil Chopra <67851308+sash9696@users.noreply.github.com>
2024-03-23 21:52:38 +0530

    Add files via upload

---
+++ node/cli/utils.js
@@ -0,0 +1,15 @@
+
+
+// export const count  = num => num + 1;
+
+// export default {name: "john"};
+//es6 modules
+
+
+// exports.count = num => num;
+
+// module.exports = {
+//     count
+// }
+//common js modules
+
